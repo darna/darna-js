7742558402cd42d25681573432ef8718987aaddc
Ken-Lauren Daganio <akosiken@gmail.com>
2014-10-03 21:42:16 +0800

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# darna-js
+
+[![Build Status](https://secure.travis-ci.org/kd/darna-js.png?branch=master)](http://travis-ci.org/kd/darna-js)
+
+It does this one thing really awesome! boyah!
---
+++ test/spec.js
@@ -0,0 +1,17 @@
+/**
+ * darna-js
+ *
+ *    Library test
+ */
+
+'use strict'
+
+var assert = require('assert'),
+lib        = require('../lib/darna-js');
+
+describe('Basic library test', function() {
+  it('should answer all questions with YO!', function() {
+    var answer = lib.darna-js('Should I tickle this unicorn?');
+    assert.equal(answer,'YO!');
+  })
+})
